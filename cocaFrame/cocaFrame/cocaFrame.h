495a450e5b2cae7a3ef4641a570f7d7e0388005f
Mahmoud-Heshmat <MahmoudHeshmat159@yahoo.com>
2018-04-29 16:12:00 +0200

    v 1

---
+++ cocaFrame/cocaFrame/cocaFrame.h
@@ -0,0 +1,19 @@
+//
+//  cocaFrame.h
+//  cocaFrame
+//
+//  Created by Ahmad ALGhamdi on 4/29/18.
+//  Copyright © 2018 Ahmad ALGhamdi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cocaFrame.
+FOUNDATION_EXPORT double cocaFrameVersionNumber;
+
+//! Project version string for cocaFrame.
+FOUNDATION_EXPORT const unsigned char cocaFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cocaFrame/PublicHeader.h>
+
+
---
+++ cocaFrame/cocaFrame/service.swift
@@ -0,0 +1,15 @@
+//
+//  service.swift
+//  cocaFrame
+//
+//  Created by Ahmad ALGhamdi on 4/29/18.
+//  Copyright © 2018 Ahmad ALGhamdi. All rights reserved.
+//
+
+import Foundation
+
+class service {
+    func setService() {
+        print("My first cocaPods")
+    }
+}
